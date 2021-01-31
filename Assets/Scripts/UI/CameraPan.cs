f780dcf19ebbf87bc0f3594881b152edcea591b2
Hitoshi Kanno <hkannok@gmail.com>
2021-01-31 13:56:46 -0500

    added camera pan with mouse

--- Assets/Scripts/UI/CameraPan.cs
+++ Assets/Scripts/UI/CameraPan.cs
@@ -16,16 +16,34 @@ public class CameraPan : MonoBehaviour
         StartinPos = transform.position;
     }
 
+    private Vector3 PreviousMousePos;
+
     // Update is called once per frame
     void Update()
     {
+
+
+
         float h = Input.GetAxis("Horizontal");
+
+        if (h == 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mouseDelta = Input.mousePosition - PreviousMousePos;
+                h = mouseDelta.x;
+            }
+        }
+
+
 
         Vector3 pos = transform.position;
         pos.x += h * PanSpeed * Time.deltaTime;
         pos.x = Mathf.Clamp(pos.x, LeftLimit.position.x, RightLimit.position.x);
 
         transform.position = pos;
+
+        PreviousMousePos = Input.mousePosition;
     }
 
     public void ResetCameraPan()
